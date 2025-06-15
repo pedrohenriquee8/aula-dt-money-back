e37df88a2aed6ee8c797e91e881ea977cbc9d258
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-06-15 18:01:36 -0300

    feat(transaction): Add new functions to transaction controller and service

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import {
   Controller,
   Get,
@@ -29,25 +32,53 @@ export class TransactionController {
   }
 
   @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  async findAll(@Res() res: Response) {
+    const transactions = await this.transactionService.findAll();
+
+    if (!transactions || !transactions.length) {
+      return res.status(204).send({ message: 'No transactions found.' });
+    }
+
+    return res.status(200).send(transactions);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
+  async findOne(@Param('id') id: string, @Res() res: Response) {
+    const transaction = await this.transactionService.findOne(id);
+
+    if (!transaction) {
+      return res.status(204).send({ message: 'No transaction found.' });
+    }
+
+    return res.status(200).send(transaction);
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateTransactionDto: UpdateTransactionDto,
+    @Res() res: Response,
   ) {
-    return this.transactionService.update(+id, updateTransactionDto);
+    const transaction = await this.transactionService.update(
+      id,
+      updateTransactionDto,
+    );
+
+    if (!transaction) {
+      return res.status(204).send({ message: 'No transaction found.' });
+    }
+
+    return res.status(200).send(transaction);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    const transaction = await this.transactionService.remove(id);
+
+    if (!transaction) {
+      return res.status(204).send({ message: 'No transaction found.' });
+    }
+
+    return res.status(200).send(transaction);
   }
 }
--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
@@ -16,22 +21,48 @@ export class TransactionService {
         type,
       },
     });
+
     return createdTransaction;
   }
 
-  findAll() {
-    return `This action returns all transaction`;
+  async findAll() {
+    const transactions = await this.prisma.transaction.findMany({
+      orderBy: {
+        data: 'desc',
+      },
+    });
+
+    return transactions;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} transaction`;
+  async findOne(id: string) {
+    const transaction = await this.prisma.transaction.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return transaction;
   }
 
-  update(id: string, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+  async update(id: string, updateTransactionDto: UpdateTransactionDto) {
+    const updatedTransaction = await this.prisma.transaction.update({
+      where: {
+        id,
+      },
+      data: updateTransactionDto,
+    });
+
+    return updatedTransaction;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} transaction`;
+  async remove(id: string) {
+    const deletedTransaction = await this.prisma.transaction.delete({
+      where: {
+        id,
+      },
+    });
+
+    return deletedTransaction;
   }
 }
